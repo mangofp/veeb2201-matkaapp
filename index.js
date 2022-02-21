0f1fa53726395470a7fe7bab6f3c20a7c21704cd
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-02-21 13:30:38 +0200

    registreerumise lehe parandused

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const matk1 = {
   id: 0,
   nimetus: "Rattamakt Jõgevamaal",
   kirjeldus: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore minima, nemo tempora rem corrupti sequi architecto eius fuga magnam temporibus dolor quam et, omnis illum officia fugit voluptatum perspiciatis! Est.",
-  pildiUrl: "./assets/rattamatk.jpg",
+  pildiUrl: "/assets/rattamatk.jpg",
   osalejad: ['mati@matkaja.ee', 'kati@matkaja.ee']
 }
 
@@ -14,7 +14,7 @@ const matk2 = {
   id: 1,
   nimetus: "Süstamatk Kõrvemaal",
   kirjeldus: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore minima, nemo tempora rem corrupti sequi architecto eius fuga magnam temporibus dolor quam et, omnis illum officia fugit voluptatum perspiciatis! Est.",
-  pildiUrl: "./assets/syst1.jpg",
+  pildiUrl: "/assets/syst1.jpg",
   osalejad: ['klaabu@suurmeri.ee']
 }
 
@@ -25,14 +25,14 @@ const matkad = [
     id: 3,
     nimetus: "Matk Vändra metsades",
     kirjeldus: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore minima, nemo tempora rem corrupti sequi architecto eius fuga magnam temporibus dolor quam et, omnis illum officia fugit voluptatum perspiciatis! Est.",
-    pildiUrl: "./assets/matkaja.png",  
+    pildiUrl: "/assets/matkaja.png",  
     osalejad: []  
   }
 ]
 
 function naitaRegistreerimist(req, res) {
   const index = parseInt(req.params.matk)
-  console.log("valitud matk" + index)
+  console.log("valitud matk " + index)
   console.log(matkad[index])
   res.render('pages/registreerumine', {matk: matkad[index]})
 }
@@ -43,5 +43,5 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index', {matkad: matkad}))
   .get('/kontakt', (req, res) => res.render('pages/kontakt'))
-  .get('/regist/:matk', naitaRegistreerimist)
+  .get('/registreerumine/:matk', naitaRegistreerimist)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
