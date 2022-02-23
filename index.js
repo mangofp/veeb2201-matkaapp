895cd9836ab2f46926985d99ceda32ab56ba8305
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-02-23 11:49:26 +0200

    kodutöö 13

--- index.js
+++ index.js
@@ -30,6 +30,46 @@ const matkad = [
   }
 ]
 
+let uudised = [
+  {
+    id:0,
+    pealkiri: "Uudis 1",
+    kokkuvote: 'Lühike tekst',
+    tekst: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas dolore fugiat earum cum libero exercitationem fugit facere voluptatibus incidunt, illo iste eos. Facilis veritatis quos molestias dicta itaque rerum!",
+    pildiUrl: "/assets/syst_krabi.jpg",
+  },
+  {
+    id:1,
+    pealkiri: "Uudis 2",
+    kokkuvote: 'Lühike tekst',
+    tekst: `
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      </p>
+      <p>
+        Laudantium quas dolore fugiat earum cum 
+        libero <strong>exercitationem fugit</strong> facere voluptatibus incidunt, 
+        illo iste eos. Facilis veritatis quos molestias dicta 
+      </p>
+      <h4>
+        Tavaline alapealkiri
+      </h4>
+      <p>
+        itaque rerum!
+      </p>  
+        `
+      ,
+    pildiUrl: "/assets/syst1.jpg",
+  },
+  {
+    id:3,
+    pealkiri: "Uudis 3",
+    kokkuvote: 'Lühike tekst',
+    tekst: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas dolore fugiat earum cum libero exercitationem fugit facere voluptatibus incidunt, illo iste eos. Facilis veritatis quos molestias dicta itaque rerum!",
+    pildiUrl: "/assets/syst_krabi.jpg",
+  }
+]
+
 function naitaRegistreerimist(req, res) {
   const index = parseInt(req.params.matk)
   console.log("valitud matk " + index)
@@ -43,5 +83,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index', {matkad: matkad}))
   .get('/kontakt', (req, res) => res.render('pages/kontakt'))
+  .get("/uudised", (req, res) => res.render("pages/uudised", { uudised: uudised }))
   .get('/registreerumine/:matk', naitaRegistreerimist)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
